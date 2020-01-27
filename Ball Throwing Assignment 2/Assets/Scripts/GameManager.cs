be33f64e961d90c64a99f90ec21bb51722468f45
PeterSeatter <56239159+PeterSeatter@users.noreply.github.com>
2020-01-27 19:37:09 +1300

    assignment 2

---
+++ Ball Throwing Assignment 2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    private float _timePassed;
+
+    public float TimePassed
+    {
+        get { return _timePassed; }
+        set { _timePassed = value; }
+    }
+
+    public float maxTimeSeconds = 5 * 60; // In seconds.
+
+
+    void Start()
+    {
+        TimePassed = 0;
+    }
+
+    void Update()
+    {
+        TimePassed += Time.deltaTime;
+
+        if (TimePassed >= maxTimeSeconds)
+        {
+            TimePassed = 0; // maxTimeSeconds;
+            SceneManager.LoadScene(0);
+        }
+    }
+}
+
+
---
+++ Ball Throwing Assignment 2/Assets/Scripts/UpdateUi.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateUi : MonoBehaviour{
+
+    public Text ScoreText;
+    public static int ScoreDisplay = 0;
+    private Player playerScript;
+
+	// Use this for initialization
+    void Start () 
+    {
+        SetScore(0);
+	}
+
+    void Awake()
+    {
+        playerScript = GetComponent<Player>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void SetScore(int prScoreToAdd)
+    {
+        try
+        {
+            //Player NewScore = GetComponent<Player>();
+            //ScoreDisplay = NewScore.Score;
+            ScoreDisplay = playerScript.Score;
+        }
+        catch (System.Exception)
+        {
+            Debug.Log("This New Score isn't working");
+        }
+            string DisplayScore = ("Score: " + ScoreDisplay);
+            ScoreText.GetComponent<Text>().text = DisplayScore;
+
+    }
+}
